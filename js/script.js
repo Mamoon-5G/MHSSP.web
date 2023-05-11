98d1b38b847b1dd53c9e9c34295804b004dbf6c0
mamoon-4g <112717860+mamoon-4g@users.noreply.github.com>
2023-05-12 02:16:44 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let nav = document.querySelector("#navbar");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+
+}
+let jump = document.querySelectorAll(".card img");
+jump.addEventListener("click", function(){
+    window.href = "staff.html"
+})
